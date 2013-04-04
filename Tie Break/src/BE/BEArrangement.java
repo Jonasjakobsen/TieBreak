36595ea173b992fffae39198e092fc9406116e32
Christian Juhl <juhlchr@gmail.com>
2013-04-04 10:34:42 +0200

    Updated

--- Tie Break/src/BE/BEArrangement.java
+++ Tie Break/src/BE/BEArrangement.java
@@ -8,7 +8,55 @@ package BE;
  *
  * @author Stoffer
  */
-public class BEArrangement
+public class BEArrangement {
+
+    private final int id;
+    private String arrangementbeskrivelse;
+    private String arrangementdag;
+    private int medlemsnr;
+
+    /**
+     *
+     * @param id
+     * @param arrangementbeskrivelse
+     * @param arrangementdag
+     * @param medlemsnr
+     */
+    public BEArrangement(int id, String arrangementbeskrivelse,
+            String arrangementdag, int medlemsnr)
 {
-    
+    this.id = id;
+    this.arrangementbeskrivelse = arrangementbeskrivelse;
+    this.arrangementdag = arrangementdag;
+    this.medlemsnr = medlemsnr;
+}
+
+    /**
+     * @return the arrangementbeskrivelse
+     */
+    public String getArrangementbeskrivelse() {
+        return arrangementbeskrivelse;
+    }
+
+    /**
+     * @return the arrangementdag
+     */
+    public String getArrangementdag() {
+        return arrangementdag;
+    }
+
+    /**
+     * @return the medlemsnr
+     */
+    public int getMedlemsnr() {
+        return medlemsnr;
+    }
+
+    /**
+     * @param medlemsnr the medlemsnr to set
+     */
+    public void setMedlemsnr(int medlemsnr) {
+        this.medlemsnr = medlemsnr;
+    }
+
 }

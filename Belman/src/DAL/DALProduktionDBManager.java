96eb67ddfedbf35a0692ac2444734389b1761538
Christoffer <Christoffer@StofferStation>
2013-05-03 15:14:51 +0200

    visprodutkion

--- Belman/src/DAL/DALProduktionDBManager.java
+++ Belman/src/DAL/DALProduktionDBManager.java
@@ -124,19 +124,21 @@ public class DALProduktionDBManager extends DALBelmanDBManager {
             ArrayList<BEProduktion> ordrer = new ArrayList<>();
 
             while (rs.next()) {
-                int sOrderId = rs.getInt("sOrderId");
-                int pOrderId = rs.getInt("pOrderId");
-                String pOrder = rs.getString("pOrder");
-                Date dueDate = rs.getDate("dueDate");
-                Float quantity = rs.getFloat("quantity");
-                Float width = rs.getFloat("width");
-                Float thickness = rs.getFloat("thickness");
-                String status = rs.getString("status");
-                boolean urgent = rs.getBoolean("urgent");
+                int SOrderID = rs.getInt("SOrderID");
+                String SOrder = rs.getString("SOrder");
+                int POrderID = rs.getInt("POrderID");
+                String POrder = rs.getString("POrder");
+                boolean Urgent = rs.getBoolean("Urgent");
+                Date DueDate = rs.getDate("DueDate");
+                Float Quantity = rs.getFloat("Quantity");
+                Float MaterialID = rs.getFloat("MaterialID");
+                Float Thickness = rs.getFloat("Thickness");
+                Float Width = rs.getFloat("Width");
+                Float Circumference = rs.getFloat("Circumference");
                 
 
 
-                BEProduktion l = new BEProduktion(sOrderId, pOrderId, pOrder, dueDate, quantity, width, thickness, status, urgent);
+                BEProduktion l = new BEProduktion(SOrderID, SOrder, POrderID, POrder, Urgent, DueDate, Quantity, MaterialID, Thickness, Width, Circumference);
                 ordrer.add(l);
             }
             return ordrer;
